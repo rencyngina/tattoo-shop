dae36c83a359a37774fcf66d9343c976a1d2b9ef
rencyngina <rencyngina@gmail.com>
2024-12-12 14:38:15 +0300

    issue deploying

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react"
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Navbar from "../Components/Navbar";
@@ -6,9 +7,9 @@ import Sectionabout from "../Components/section-about";
 import Sectionservices from "../Components/sectionservices";
 import Foot from "../Components/foot";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  
   return (
    <main>
       <Navbar />
