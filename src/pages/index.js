34e9255d1a1f5699227514cae3725ea14d841008
rencyngina <rencyngina@gmail.com>
2024-12-12 10:42:46 +0300

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import Navbar from "@/Components/Navbar";
-import Hero from "@/Components/HeroSection/Hero";
-import Sectionabout from "@/Components/section-about";
-import Sectionservices from "@/Components/sectionservices";
-import Foot from "@/Components/foot";
+import Navbar from "/Components/Navbar";
+import Hero from "/Components/HeroSection/Hero";
+import Sectionabout from "/Components/section-about";
+import Sectionservices from "/Components/sectionservices";
+import Foot from "/Components/foot";
 
 const inter = Inter({ subsets: ["latin"] });
 
